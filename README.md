c2a66342aaa90045f270210bef06793391f1535a
Ravi Mariya <ravimariya9492@gmail.com>
2018-05-21 11:40:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# SimpleNumbers
+
+SimpleNumbers is a simple Gem to format numbers from number to usd, indian rs., number to percentage, number to phone, number to storage size and number to human number easily from view, controller or model
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+```ruby
+gem 'simple_numbers'
+```
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install simple_numbers
+
+## Usage
+
+```ruby
+123456789.inr #=>  "Rs. 123,456,789.00"
+123456789.usd # => "$123,456,789.00"
+1234567890.phone # => "123-456-7890"
+12345.size #=> "12.1 KB"
+100.percentage #=> "100.000%"
+```
+for more options [Number Helper](http://api.rubyonrails.org/classes/ActionView/Helpers/NumberHelper.html)
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/ravimariya/simple_numbers. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
+
+
+## License
+
+The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
+
---
+++ lib/simple_numbers.rb
@@ -0,0 +1,58 @@
+require "simple_numbers/version"
+
+module SimpleNumbers
+  HELPER = ActionController::Base.helpers
+
+  module Format
+    def self.included(recipient)
+      recipient.extend(ClassMethods)
+      recipient.class_eval do
+        include InstanceMethods
+      end
+    end
+
+    module ClassMethods
+    end
+
+    module InstanceMethods
+      
+      # Format Money
+
+      def usd(**options)
+        options.delete(:unit) if options.key?(:unit)
+        HELPER.number_to_currency(self, options)
+      end
+
+      def inr(**options)
+        options.delete(:unit) if options.key?(:unit)
+        HELPER.number_to_currency(self, options.merge!(unit: 'Rs. '))
+      end
+
+      # Format Percentage
+
+      def percentage(**options)
+        HELPER.number_to_percentage(self, options)
+      end
+    
+      # Format Phone Number
+
+      def phone(**options)
+        HELPER.number_to_phone(self, options)
+      end
+
+      # Format Size
+
+      def size(**options)
+        HELPER.number_to_human_size(self, options)
+      end
+
+      # Format Number
+
+      def human_number(**options)
+        HELPER.number_to_human(self, options)
+      end
+    end
+  end
+end
+Fixnum.send :include, SimpleNumbers::Format
+Float.send :include, SimpleNumbers::Format
